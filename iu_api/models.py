3645068d9437ebf3a599513da6b0c23579df81e7
Azeimin <oleg-tsv@yandex.ru>
2021-05-19 18:57:23 +0300

    Changed data storaging format for UserOnlineActivityObject

--- iu_api/models.py
+++ iu_api/models.py
@@ -4,8 +4,8 @@ from django.db import models
 # Каждый объект этого класса будет хранить данные об одной игровой сессии одного пользователя
 class UserOnlineActivityObject(models.Model):
     steam_id = models.IntegerField()
-    started_playing_time = models.DateTimeField()
-    ended_playing_time = models.DateTimeField()
+    started_playing_timestamp = models.IntegerField()
+    ended_playing_timestamp = models.IntegerField()
     game_id = models.IntegerField()
     total_played = models.FloatField()
 
--- iu_api/serializers.py
+++ iu_api/serializers.py
@@ -5,4 +5,4 @@ from rest_framework import serializers
 class UserOnlineActivityObjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserOnlineActivityObject
-        fields = ['steam_id', 'started_playing_time', 'ended_playing_time', 'game_id', 'total_played']
+        fields = ['steam_id', 'started_playing_timestamp', 'ended_playing_timestamp', 'game_id', 'total_played']
