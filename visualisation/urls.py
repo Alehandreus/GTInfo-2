c04e8922a18f96f9fe25ee4792eb0be832a254e7
Azeimin <oleg-tsv@yandex.ru>
2021-07-20 16:14:16 +0300

    Added some code for retrieving tracked users info

--- visualisation/urls.py
+++ visualisation/urls.py
@@ -4,4 +4,5 @@ from .views import *
 urlpatterns = [
     path('visualisation/', VisualisationView.as_view()),
     path('visualisation/hours/', HoursView.as_view()),
+    path('get_tracked/', GetTrackedUsersView.as_view()),
 ]
--- visualisation/views.py
+++ visualisation/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, loader
 from django.views import View
-from django.http import HttpResponseRedirect, HttpResponse
-
+from django.http import HttpResponseRedirect, HttpResponse, JsonResponse
 
 class VisualisationView(View):
     def get(self, request):
@@ -19,3 +18,18 @@ class HoursView(View):
         context = {}
         template = loader.get_template("visualisation/hours.html")
         return HttpResponse(template.render(context, request))
+
+
+class GetTrackedUsersView(View):
+    def post(self, request):
+        context = {}
+        if not request.user.is_authenticated:
+            context["status"] = "unauthorized"
+        else:
+            try:
+                context["users"] = [user.steam_id for user in request.user.account.tracked_users.all()]
+                print(context)
+                context["status"] = "ok"
+            except:
+                context["status"] = "error"
+        return JsonResponse(context)
